ec95fd28f6cefb2904b5bef15ba5d9405008abba
kaganndemirr <kaganndemirr@gmail.com>
2023-04-05 10:19:52 +0300

    initial commit

---
+++ SimpleCRUD/Migrations/20230405062301_FirstMigration.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SimpleCRUD.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SecondName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    From = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    isPersonnel = table.Column<bool>(type: "bit", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    InstutionNumber = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
---
+++ SimpleCRUD/Models/Person.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleCRUD.Models
+{
+    public class Person
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [DisplayName("First Name")]
+        [Required(ErrorMessage = "This field is required")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [DisplayName("Second Name")]
+        public string SecondName { get; set; } = string.Empty;
+
+        [DisplayName("Last Name")]
+        [Required(ErrorMessage = "This field is required")]
+        public string LastName { get; set; } = string.Empty;
+
+        [DisplayName("Birthday")]
+        public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        public string Phone { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Address { get; set; } = string.Empty;
+
+        public string From { get; set; } = string.Empty;
+
+        [DisplayName("Personnel ?")]
+        [Required(ErrorMessage = "This field is required")]
+        public bool isPersonnel { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "This field is required")]
+        public int InstutionNumber { get; set; }
+
+
+    }
+}
