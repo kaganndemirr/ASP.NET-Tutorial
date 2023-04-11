080842210ee316165cc6d8b249324d972dbedbf7
kaganndemirr <kaganndemirr@gmail.com>
2023-04-11 11:17:50 +0300

    initial commit

---
+++ IdentityTutorial/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IdentityTutorial.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
